473e0777bd379df35b568211591962c3eb43249c
Enzo Araujo <enzo.xsti@gmail.com>
2026-05-19 11:26:26 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 CORS(app,
-     origins=["http://localhost:5173", "http://10.92.3.126:5173"],
+     origins=["http://localhost:5173", "http://10.92.3.147:5173"],
      supports_credentials=True
 )
 
